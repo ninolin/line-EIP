2088c82fb418a45719bbe0cb4c3c3707db802890
ninolin <a7872278722@gmail.com>
2019-04-23 23:04:03 +0800

    發現線上環境middleware auth有錯，先不使用

--- routes/web.php
+++ routes/web.php
@@ -17,11 +17,11 @@ Route::get('/', function () {return view('login');})->name('login');
 Route::get('/applyleave', 'View\applyleave@create');
 Route::get('/validateleave', 'View\validateleave@create');
 Route::get('/leavelog', 'View\leavelog@create')->name('leavelog');
-Route::middleware('auth')->get('/whmanage', 'View\whmanage@index')->name('whmanage');
-Route::middleware('auth')->get('/userlist', 'View\userlist@create')->name('userlist');
-Route::middleware('auth')->get('/titlelist', 'View\titlelist@create')->name('titlelist');
-Route::middleware('auth')->get('/leavetypelist', 'View\leavetypelist@create')->name('leavetypelist');
-Route::middleware('auth')->get('/overworktypelist', 'View\overworktypelist@create')->name('overworktypelist');
-Route::middleware('auth')->get('/formmanage', function () {return view('contents.formmanage');})->name('formmanage');
+//Route::middleware('auth')->get('/whmanage', 'View\whmanage@index')->name('whmanage');
+Route::get('/userlist', 'View\userlist@create')->name('userlist');
+Route::get('/titlelist', 'View\titlelist@create')->name('titlelist');
+Route::get('/leavetypelist', 'View\leavetypelist@create')->name('leavetypelist');
+Route::get('/overworktypelist', 'View\overworktypelist@create')->name('overworktypelist');
+Route::get('/formmanage', function () {return view('contents.formmanage');})->name('formmanage');
 Route::post('/login', 'Auth\LoginController@login')->name('doLogin');//驗證密碼
 
