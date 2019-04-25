70c7fe243f1baae5a5e6fecf238273a9c5c1cc1a
ninolin <a7872278722@gmail.com>
2019-04-25 22:44:50 +0800

    bug fixed

--- app/Providers/LineServiceProvider.php
+++ app/Providers/LineServiceProvider.php
@@ -33,6 +33,7 @@ class LineServiceProvider extends ServiceProvider
     {
         //尋找該用戶所屬line_channel的access_token
         $channel_array = config('line.channel');
+        log::info($channel_array);
         $line_channel = "";
         $line_channel_access_token = "";
         $users = DB::select('select line_channel from user where line_id = ?', [$line_id]);
