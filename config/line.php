4da2e43221b33ae0a348b32072e03eabff3633c3
ninolin <a7872278722@gmail.com>
2019-04-25 22:57:14 +0800

    test

--- config/line.php
+++ config/line.php
@@ -2,7 +2,6 @@
 
 return [
 
-    //'channel' => env('LINE_CHANNEL') ? array_filter(explode(',', env('LINE_CHANNEL'))) : []
-    'channel' => env('LINE_CHANNEL')
+    'channel' => env('LINE_CHANNEL') ? array_filter(explode(',', env('LINE_CHANNEL'))) : []
 
 ];
