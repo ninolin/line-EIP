1b9729b3f50afdb43aae7ac6fe90e56e0eccbcdf
ninolin <a7872278722@gmail.com>
2019-04-25 23:00:41 +0800

    test

--- config/line.php
+++ config/line.php
@@ -2,6 +2,6 @@
 
 return [
 
-    'channel' => env('LINE_CHANNEL') ? array_filter(explode(',', env('LINE_CHANNEL'))) : []
+    'channel' => env('LINE_CHANNEL') ? explode(',', env('LINE_CHANNEL')) : []
 
 ];
