017ec1efbad8f048e45ea7edac84825730ab29a4
ninolin <a7872278722@gmail.com>
2019-05-19 18:43:40 +0800

    bug fixed

--- app/Http/Controllers/Line/Receive.php
+++ app/Http/Controllers/Line/Receive.php
@@ -19,41 +19,43 @@ class Receive extends Controller
         $json_obj = json_decode($bodyContent); //轉成json格式
         $sender_replyToken = $json_obj->events[0]->replyToken; //取得訊息的replyToken
         $sender_userid = $json_obj->events[0]->source->userId; //取得訊息發送者的id
-        $sender_msg_type = $json_obj->events[0]->type; //取得訊息的type
-        if($sender_msg_type == "message") {
-            $sender_txt = $json_obj->events[0]->message->text; //取得訊息內容
-            $user = DB::select('select * from user where line_id =?', [$sender_userid]);
-            if(count($user) == 0) {
-                //該line_id無在db中存在，判斷是不是一個md5字串，是的話就是要進行綁定，否的話就請輸入認證碼
-                if (preg_match("/[a-z0-9]{32}/", $sender_txt)) {
-                    $unlink_user = DB::select("select * from user where status = 'T' and (line_id = '' or line_id is null) and MD5(CONCAT(NO, dd)) = ?", [$sender_txt]);
-                    foreach ($unlink_user as $v) {
-                        if(DB::update("update user set line_id =?, line_channel = ? where NO =?", [$sender_userid, $line_channel, $v->NO]) == 1) {
-                            LineServiceProvider::pushTextMsg($sender_userid, "恭喜".$v->cname."成功加入，歡迎使用");
-                        } else {
-                            LineServiceProvider::replyTextMsgWithChannel($sender_userid, $sender_replyToken, $line_channel, "綁定失敗:更新db失敗");
+        $sender_type = $json_obj->events[0]->type; //取得訊息的type
+        if($sender_type == "message") {
+            if( $json_obj->events[0]->message->type == "text") {
+                $sender_txt = $json_obj->events[0]->message->text; //取得訊息內容
+                $user = DB::select('select * from user where line_id =?', [$sender_userid]);
+                if(count($user) == 0) {
+                    //該line_id無在db中存在，判斷是不是一個md5字串，是的話就是要進行綁定，否的話就請輸入認證碼
+                    if (preg_match("/[a-z0-9]{32}/", $sender_txt)) {
+                        $unlink_user = DB::select("select * from user where status = 'T' and (line_id = '' or line_id is null) and MD5(CONCAT(NO, dd)) = ?", [$sender_txt]);
+                        foreach ($unlink_user as $v) {
+                            if(DB::update("update user set line_id =?, line_channel = ? where NO =?", [$sender_userid, $line_channel, $v->NO]) == 1) {
+                                LineServiceProvider::pushTextMsg($sender_userid, "恭喜".$v->cname."成功加入，歡迎使用");
+                            } else {
+                                LineServiceProvider::replyTextMsgWithChannel($sender_userid, $sender_replyToken, $line_channel, "綁定失敗:更新db失敗");
+                            }
                         }
-                    }
+                    } else {
+                        LineServiceProvider::replyTextMsgWithChannel($sender_userid, $sender_replyToken, $line_channel, "歡迎初次使用EIP系統，請輸入認證碼來讓我知道你是誰");
+                    }            
                 } else {
-                    LineServiceProvider::replyTextMsgWithChannel($sender_userid, $sender_replyToken, $line_channel, "歡迎初次使用EIP系統，請輸入認證碼來讓我知道你是誰");
-                }            
-            } else {
-                if($sender_txt == "log") {
-                    LineServiceProvider::sendIndividualLogFlexMeg($sender_userid);
+                    if($sender_txt == "log") {
+                        LineServiceProvider::sendIndividualLogFlexMeg($sender_userid);
+                    }
+                    LineServiceProvider::replyTextMsgWithChannel($sender_userid, $sender_replyToken, $line_channel, $sender_txt);
                 }
-                LineServiceProvider::replyTextMsgWithChannel($sender_userid, $sender_replyToken, $line_channel, $sender_txt);
+            } else if( $json_obj->events[0]->message->type == "image") {
+                $image_id = $json_obj->events[0]->message->id; //取得圖片訊息編號
+                $filename = LineServiceProvider::getImage($sender_userid, $image_id);
             }
-        } else if ($sender_msg_type == "postback") {
+            
+        } else if ($sender_type == "postback") {
             $postback_data = $json_obj->events[0]->postback->data; //取得訊息內容
             $action = explode("&",$postback_data)[0];
             if($action == "show_apply_detail") {
                 LineServiceProvider::sendShowApplyDetailFlexMeg($sender_userid, explode("&",$postback_data)[1]);
             }
-        } else if ($sender_msg_type == "image") {
-            $image_id = $json_obj->events[0]->message->id; //取得圖片訊息編號
-            $filename = LineServiceProvider::getImage($sender_userid, $image_id);
-            
-        }
+        } 
         
         return response()->json([
             'status' => 'successful',
