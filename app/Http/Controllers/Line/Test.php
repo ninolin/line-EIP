8cab01c672cd7b3e0a226519829cc471afd77be7
nino <ninolin@ioneit.com>
2019-06-10 23:47:08 +0800

    增加用google calendar計算請假

--- app/Http/Controllers/Line/Test.php
+++ app/Http/Controllers/Line/Test.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
 use App\Providers\HelperServiceProvider;
 use DateTime;
-
+use Log;
 class Test extends Controller
 {
     /**
@@ -49,13 +49,48 @@ class Test extends Controller
      */
     public function show()
     {
-        $d = new DateTime();
-        $timeMin = rawurlencode($d->format('Y-m-d\TH:i:sP'));
-        HelperServiceProvider::get_req("https://www.googleapis.com/calendar/v3/calendars/zh-tw.taiwan%23holiday%40group.v.calendar.google.com/events?key=AIzaSyB0ZMfTWE_h_qAVNRWpZFnDUOPaiT-a7xo&timeMin=".$timeMin);
+        // $d = new DateTime();
+        // $timeMin = rawurlencode($d->format('Y-m-d\TH:i:sP'));
+        // HelperServiceProvider::get_req("https://www.googleapis.com/calendar/v3/calendars/zh-tw.taiwan%23holiday%40group.v.calendar.google.com/events?key=AIzaSyB0ZMfTWE_h_qAVNRWpZFnDUOPaiT-a7xo&timeMin=".$timeMin);
         
+        $return = self::is_offday_by_gcalendar("2019-06-08");
+        $dates = self::dates2array("2019-06-08", "2019-06-10");
+        $hours = 0;
+        foreach ($dates as $e) {
+            //log::info(self::is_offday_by_gcalendar($e));
+            $hours += self::is_offday_by_gcalendar($e);
+        }
         return response()->json([
-            'status' => 's'
+            'status' => $hours
         ]);
+    }
+    static protected function dates2array($date1, $date2) {
+        $return= array();
+        $diff_date = (strtotime($date2) - strtotime($date1))/ (60*60*24); //計算相差之天數
+        for ($i=0; $i<=$diff_date; $i++) {
+            array_push($return, date('Y-m-d', strtotime('+'.$i.' days', strtotime($date1))));
+        }
+        return $return;
+    }
+
+    static protected function is_offday_by_gcalendar($check_date) {
+        $timeMin = rawurlencode($check_date."T00:00:00Z");
+        $timeMax = rawurlencode(date('Y-m-d', strtotime('+1 days', strtotime($check_date)))."T00:00:00Z");
+        $calevents_str = HelperServiceProvider::get_req("https://www.googleapis.com/calendar/v3/calendars/nino.dev.try%40gmail.com/events?key=AIzaSyB0ZMfTWE_h_qAVNRWpZFnDUOPaiT-a7xo&timeMin=".$timeMin."&timeMax=".$timeMax);
+        $calevents = json_decode($calevents_str) -> items;
+        //return $calevents;
+        $offhours = 0;
+        foreach ($calevents as $e) {
+            if($e-> status == "confirmed") {
+                if(strpos($e-> summary,'休息日') !== false && $e-> start-> date == $check_date) {
+                    $offhours = $offhours + 8;
+                }
+            }
+            if($e-> status == "cancelled") {
+                $offhours = $offhours - 8;
+            }
+        }
+        return $offhours;
     }
 
     /**
--- app/Http/Controllers/View/applyleave.php
+++ app/Http/Controllers/View/applyleave.php
@@ -25,17 +25,16 @@ class applyleave extends Controller
 
     /**
      * 顯示applyleave頁面
-     *
-     * @return \Illuminate\Http\Response
      */
     public function create()
     {
         
-        //因為有同一種假但不同天數的假別，所以做distinct name，之後新增假時，再判斷是用那一種假的id
-        $sql = 'select distinct elt.name, et.name as title_name, et.id as title_id ';
+        //因為有同一種假但不同天數的假別，所以做distinct name，之後新增假時，再判斷是用那一種假別的id
+        $sql  ='select distinct elt.name, et.name as title_name, et.id as title_id ';
         $sql .='from eip_leave_type elt, eip_title et ';
         $sql .='where elt.approved_title_id = et.id ';
         $leavetypes = DB::select($sql, []);
+        //用戶只列出有加入line的而且是status是T(True)
         $users = DB::select("select * from user where status = 'T' and line_id != '' order by cname", []);
         return view('line.applyleave', [
             'leavetypes' => $leavetypes,
@@ -45,19 +44,18 @@ class applyleave extends Controller
     }
 
     /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * 新增一筆請假紀錄
+     * 4個step:
+     * step1: 取得要寫入請假相關table的資料(假別id,申請人、代理人和第一簽核人資料)
+     * step2: 計算請假小時
+     * step3: 寫入請假和簽核流程table
+     * step4: 通知申請人、代理人和第一簽核人
+     * 
+     * @param \Illuminate\Http\Request
      */
     public function store(Request $request)
     {
         try {
-            /*
-             * 2個step:
-             * step1: 寫入請假紀錄
-             * step2: 通知申請人、代理人、第一簽核人
-            */
             $apply_user_line_id = $request->get('userId');      //申請者的line_id
             $leave_agent_user_no = $request->get('leaveAgent'); //代理人的user_NO
             $leavename = $request->get('leaveType');            //假別名稱
@@ -68,7 +66,7 @@ class applyleave extends Controller
             $comment = $request->input('comment');              //備註
             if($comment == "") $comment = "-";
 
-            //echo $leave_days;
+            //取得假別的id
             $leave_type_arr = DB::select('select * from eip_leave_type where name =? order by day ASC', [$leavename]);
             $leave_type_id = "";
             $i = 0;
@@ -110,33 +108,31 @@ class applyleave extends Controller
                 $upper_user_no = $v->NO; 
             }
             if($upper_line_id == "") throw new Exception('請假失敗:未設定簽核人或簽核人的line未加入EIP中');
-            //計算請假小時
-            $leave_hours = (strtotime($end_date) - strtotime($start_date))/60/60;
-            if($leave_hours == 0) { //當天請假
-                $leave_hours = (strtotime($end_time) - strtotime($start_time))/(60*60);
-                if($leave_hours >= 4) {
-                    $leave_hours--;
+            //計算請假小時(目前先預設工時是早8晚5,30m為1單位)
+            $dates = self::dates2array($start_date, $end_date);
+            $leave_hours = 0;
+            if(count($dates) == 1) { 
+                //只請一天
+                if(self::is_offday_by_gcalendar($start_date) == 8) { //先確定當天是不是休息日，不是休息日的話再來算請假小時
+                    $leave_hours += self::cal_timediff($start_time, $end_tim);
                 }
-            } else if($leave_hours == 24) {
-                $leave_hours = ((strtotime("17:00") - strtotime($start_time))/(60*60)) + ((strtotime($end_time) - strtotime("08:00"))/(60*60));
-            } else {
-                date('Y-m-d', strtotime('+1 days', strtotime($start_time)));
-            }
-            $d = new DateTime($start_date);
-            $timeMin = rawurlencode($start_date."T00:00:00Z");
-            $d = new DateTime($end_date);
-            $timeMax = rawurlencode($d->format('Y-m-d\TH:i:sP'));
-            $calevents_str = HelperServiceProvider::get_req("https://www.googleapis.com/calendar/v3/calendars/nino.dev.try%40gmail.com/events?key=AIzaSyB0ZMfTWE_h_qAVNRWpZFnDUOPaiT-a7xo&timeMin=".$timeMin."&timeMax=".$timeMax);
-            $calevents = json_decode($calevents_str) -> items;
-            $leave_days = 0;
-            foreach ($calevents as $v) {
-                if($v->summary == "休息日"){
-                    
+            } else {    
+                //請超過一天
+                foreach ($dates as $key=>$d) {
+                    if($key == 0) {
+                        if(self::is_offday_by_gcalendar($start_date) == 8) {
+                            $leave_hours += self::cal_timediff($start_time, "17:00");
+                        }
+                    } else if($key == count($dates)-1) {
+                        if(self::is_offday_by_gcalendar($end_date) == 8) {
+                            $leave_hours += self::cal_timediff("08:00", $end_time);
+                        }
+                    } else {
+                        $leave_hours += self::is_offday_by_gcalendar($d);
+                    }
                 }
-                $agent_line_id = $v->line_id;
             }
-            //$leave_days = round((strtotime($end_date)-strtotime($start_date))/3600/24); //請假天數
-
+            
             //寫入請假紀錄
             $sql = "insert into eip_leave_apply ";
             $sql .= "(apply_user_no, apply_type, agent_user_no, type_id, start_date, start_time, end_date, end_time, comment) ";
@@ -204,36 +200,60 @@ class applyleave extends Controller
     }
 
     /**
-     * Show the form for editing the specified resource.
+     * 回傳2個時間間的相差小時，中午自動休一小時
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param  string  $time1
+     * @param  string  $time2
+     * @return float 
      */
-    public function edit($id)
-    {
-        //
+    static protected function cal_timediff($time1, $time2) {
+        $hours = (strtotime($time2) - strtotime($time1))/(60*60);
+        if($hours >= 5) {
+            $leave_hours = $leave_hours-1;
+        } else if($leave_hours > 4) {
+            $leave_hours = 4;
+        }
     }
 
     /**
-     * Update the specified resource in storage.
+     * 回傳2個日期間的所有日期
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param  string  $date1
+     * @param  string  $date2
+     * @return array 
      */
-    public function update(Request $request, $id)
-    {
-        //
+    static protected function dates2array($date1, $date2) {
+        $return= array();
+        $diff_date = (strtotime($date2) - strtotime($date1))/ (60*60*24); //計算相差之天數
+        for ($i=0; $i<=$diff_date; $i++) {
+            array_push($return, date('Y-m-d', strtotime('+'.$i.' days', strtotime($date1))));
+        }
+        return $return;
     }
 
     /**
-     * Remove the specified resource from storage.
+     * 檢查日期的上班小時
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param  string  $check_date
+     * @return int
      */
-    public function destroy($id)
-    {
-        //
+    static protected function is_offday_by_gcalendar($check_date) {
+        $timeMin = rawurlencode($check_date."T00:00:00Z");
+        $timeMax = rawurlencode(date('Y-m-d', strtotime('+1 days', strtotime($check_date)))."T00:00:00Z");
+        $calevents_str = HelperServiceProvider::get_req("https://www.googleapis.com/calendar/v3/calendars/nino.dev.try%40gmail.com/events?key=AIzaSyB0ZMfTWE_h_qAVNRWpZFnDUOPaiT-a7xo&timeMin=".$timeMin."&timeMax=".$timeMax);
+        $calevents = json_decode($calevents_str) -> items;
+        //return $calevents;
+        $offhours = 0;
+        foreach ($calevents as $e) {
+            if($e-> status == "confirmed") {
+                if(strpos($e-> summary,'休息日') !== false && $e-> start-> date == $check_date) {
+                    $offhours = $offhours + 8;
+                }
+            }
+            if($e-> status == "cancelled") {
+                $offhours = $offhours - 8;
+            }
+        }
+        return $offhours;
     }
 }
