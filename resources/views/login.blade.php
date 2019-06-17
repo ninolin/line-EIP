67536ce5400b391bf4b6bd85d5cd73ea74ad79b2
nino <ninolin@ioneit.com>
2019-06-17 11:27:34 +0800

    bug fixed

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -38,11 +38,13 @@
         <script src="{{ asset('js/bootstrap/js/bootstrap.min.js') }}"></script>
         <script src="https://apis.google.com/js/platform.js" async defer></script>
         <script type="text/javascript">
-            var profile = googleUser.getBasicProfile();
-            console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-            console.log('Name: ' + profile.getName());
-            console.log('Image URL: ' + profile.getImageUrl());
-            console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+            function onSignIn(googleUser) {
+                var profile = googleUser.getBasicProfile();
+                console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+                console.log('Name: ' + profile.getName());
+                console.log('Image URL: ' + profile.getImageUrl());
+                console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+            }
         </script>
     </body>
 </html>
