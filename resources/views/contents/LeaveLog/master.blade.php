5bc18f5bc702382143b70849b30097d4aebcfd19
nino <ninolin@ioneit.com>
2019-08-12 22:44:47 +0800

    更新menu的文字 & 移除不需要的檔案

--- resources/views/contents/LeaveLog/master.blade.php
+++ resources/views/contents/LeaveLog/master.blade.php
@@ -3,8 +3,8 @@
 <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #f1f3f5">
   <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
     <div class="navbar-nav">
-      <a class="nav-link @if ($tab == 'last') active @endif" href="{{ route('ll_last') }}">最近30天紀錄</a>
-      <a class="nav-link @if ($tab == 'individual') active @endif" href="{{ route('ll_individual') }}">員工紀錄</a>
+      <a class="nav-link @if ($tab == 'last') active @endif" href="{{ route('ll_last') }}">簽核中紀錄</a>
+      <a class="nav-link @if ($tab == 'individual') active @endif" href="{{ route('ll_individual') }}">員工紀錄查詢</a>
     </div>
   </div>
 </nav>
