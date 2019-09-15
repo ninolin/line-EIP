59244278d2708cc32f76c63d4f123202713cb283
nino <ninolin@ioneit.com>
2019-09-15 18:22:36 +0800

    bug fixed, #27

--- resources/views/line/validatetype.blade.php
+++ resources/views/line/validatetype.blade.php
@@ -11,7 +11,7 @@
         <link href="{{ asset('css/public.css') }}" rel="stylesheet">
     </head>
     <body style="background-color: #f1f1f1;">
-        <div class="weui-flex"><div class="weui-flex__item mobile_topbar">工時紀錄</div></div>
+        <div class="weui-flex"><div class="weui-flex__item mobile_topbar">簽核操作</div></div>
         <div class="text-center">
             <div id="useridfield" style="display:none"></div>
             <div class="weui-cells" id="validate_type_data">
@@ -31,11 +31,11 @@
         };
 
         function initializeApp(data) {
-            $html =  '<a class="weui-cell weui-cell_access" href="./validateleave/unvalidate/'+data.context.userId+'">';
+            $html =  '<a class="weui-cell weui-cell_access" href="../validateleave/unvalidate/'+data.context.userId+'">';
             $html +=  ' <div class="weui-cell__bd"><p>未簽核</p></div>';
             $html +=  ' <div class="weui-cell__ft"></div>';
             $html +=  '</a>';
-            $html +=  '<a class="weui-cell weui-cell_access" href="./validateleave/validate/'+data.context.userId+'">';
+            $html +=  '<a class="weui-cell weui-cell_access" href="../validateleave/validate/'+data.context.userId+'">';
             $html +=  ' <div class="weui-cell__bd"><p>已簽核</p></div>';
             $html +=  ' <div class="weui-cell__ft"></div>';
             $html +=  '</a>';
