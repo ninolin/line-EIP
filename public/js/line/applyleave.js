9dbfb6ca96bc385872e0630c7d2f9d1a30a39c1b
nino <ninolin@ioneit.com>
2019-10-15 23:40:48 +0800

    測試程式, #38

--- public/js/line/applyleave.js
+++ public/js/line/applyleave.js
@@ -38,48 +38,52 @@ function initializeApp(data) {
 }
 
 const setTime = (id) => {
-    const hour = $("#"+id).html().split(':')[0];
-    const min = $("#"+id).html().split(':')[1];
-    let defaultValue = [hour, min];
-    if(id == "endTime") defaultValue = [hour, min];
-    weui.picker(
-        [
-            {label: '01', value: '01'}, 
-            {label: '02', value: '02'}, 
-            {label: '03', value: '03'},
-            {label: '04', value: '04'},
-            {label: '05', value: '05'},
-            {label: '06', value: '06'},
-            {label: '07', value: '07'},
-            {label: '08', value: '08'},
-            {label: '09', value: '09'},
-            {label: '10', value: '10'},
-            {label: '11', value: '11'},
-            {label: '12', value: '12'},
-            {label: '13', value: '13'},
-            {label: '14', value: '14'},
-            {label: '15', value: '15'},
-            {label: '16', value: '16'},
-            {label: '17', value: '17'},
-            {label: '18', value: '18'},
-            {label: '19', value: '19'},
-            {label: '20', value: '20'},
-            {label: '21', value: '21'},
-            {label: '22', value: '22'},
-            {label: '23', value: '23'}
-        ], 
-        [
-            {label: '00', value: '00'}, {label: '30',value: '30'}
-        ], 
-        {
-        defaultValue: defaultValue,
-        onConfirm: function (result) {
-            $("#"+id).html(result[0].value + ":" + result[1].value);
-        },
-        id: id
-        }
-    );
+    $("#time_select").show();
 }
+// const setTime = (id) => {
+//     const hour = $("#"+id).html().split(':')[0];
+//     const min = $("#"+id).html().split(':')[1];
+//     let defaultValue = [hour, min];
+//     if(id == "endTime") defaultValue = [hour, min];
+//     weui.picker(
+//         [
+//             {label: '01', value: '01'}, 
+//             {label: '02', value: '02'}, 
+//             {label: '03', value: '03'},
+//             {label: '04', value: '04'},
+//             {label: '05', value: '05'},
+//             {label: '06', value: '06'},
+//             {label: '07', value: '07'},
+//             {label: '08', value: '08'},
+//             {label: '09', value: '09'},
+//             {label: '10', value: '10'},
+//             {label: '11', value: '11'},
+//             {label: '12', value: '12'},
+//             {label: '13', value: '13'},
+//             {label: '14', value: '14'},
+//             {label: '15', value: '15'},
+//             {label: '16', value: '16'},
+//             {label: '17', value: '17'},
+//             {label: '18', value: '18'},
+//             {label: '19', value: '19'},
+//             {label: '20', value: '20'},
+//             {label: '21', value: '21'},
+//             {label: '22', value: '22'},
+//             {label: '23', value: '23'}
+//         ], 
+//         [
+//             {label: '00', value: '00'}, {label: '30',value: '30'}
+//         ], 
+//         {
+//             defaultValue: defaultValue,
+//             onConfirm: function (result) {
+//                 console.log(result[0].value + ":" + result[1].value);
+//                 $("#"+id).html(result[0].value + ":" + result[1].value);
+//             },
+//             id: id
+//         }
+//     );
+// }
 
 const apply_leave = () => {
     
