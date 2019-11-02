2ddeed9d1064c4b917bc569c681f7deb25819d86
nino <ninolin@ioneit.com>
2019-11-02 22:08:26 +0800

    暫時隱藏不用的header

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -18,7 +18,7 @@
                     </div>
                 </ul>
             </li>
-            <li class="nav-item px-3">
+            <!-- <li class="nav-item px-3">
                 <a class="nav-link @if (Request::is('formmanage')) active @endif" href="{{ route('formmanage') }}">表單管理</a>
             </li>
             <li class="nav-item px-3">
@@ -26,7 +26,7 @@
             </li>
             <li class="nav-item px-3">
                 <a class="nav-link" href="https://themes.getbootstrap.com/">公用資源</a>
-            </li>
+            </li> -->
         </ul>
     </div>
     <div class="ml-md-auto">
